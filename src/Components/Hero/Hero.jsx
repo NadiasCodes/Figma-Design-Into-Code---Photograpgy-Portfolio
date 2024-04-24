58e181fafb91e4aaaf8bab33479c9a6a87c4c33b
NadiasCodes <De.nadia@outlook.com>
2024-04-24 14:59:17 +0200

    created the  main hero section and styled it

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import HeroImage from "../../assets/hero.png";
+import "./Hero.css";
 
 const Hero = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="hero ">
+      <div className="hero-text container-box">
+        <h2>SHELLEY WHITE</h2>
+        <h1>PHOTOGRAPHY</h1>
+        <p>
+          Nisl faucibus quis urna nibh ut amet. Diam suscipit
+          <br /> pretium massa, sollicitudin in risus in. Sagittis.
+        </p>
+        <button className="hero-btn">GET STARTED</button>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
